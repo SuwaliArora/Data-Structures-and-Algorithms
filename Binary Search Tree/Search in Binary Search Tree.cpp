5eee6823a33e42cdde843469a0e77cf6dbd1f1c7
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-04-09 11:55:13 +0530

    Iterative Approach
    https://leetcode.com/problems/search-in-a-binary-search-tree/

---
+++ Binary Search Tree/Search in Binary Search Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* temp=root;
+        while(temp!=NULL) {
+            if(temp->val == val) 
+                return temp;
+            else if(temp->val >val) 
+                temp=temp->left;
+            else
+                temp=temp->right;
+        }
+        return temp;
+    }
+};
