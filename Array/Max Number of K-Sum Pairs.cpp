a36a834aeb3cec2c7bca257076c0f573f48bfbe3
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-05-04 12:45:04 +0530

    Approach 1
    https://leetcode.com/problems/max-number-of-k-sum-pairs/

---
+++ Array/Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            // if k-nums[i] is present in the map
+            if(mp[k-nums[i]]>0){
+                count++;
+                mp[k-nums[i]]--;
+            }
+            else{
+                 mp[nums[i]]++;
+            }
+        }
+        return count;
+    }
+};
