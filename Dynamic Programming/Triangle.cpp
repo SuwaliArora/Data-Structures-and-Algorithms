cc34eed915c87248cde6c859631da459ee117292
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-05-20 23:11:08 +0530

    Dp
    https://leetcode.com/problems/triangle/

---
+++ Dynamic Programming/Triangle.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        // traverse the rows
+        for(int i=triangle.size()-2;i>=0;i--){
+            // traverse the columns
+            for(int j=0;j<=i;j++){
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
+            }
+        }
+        return triangle[0][0];
+    }
+};
