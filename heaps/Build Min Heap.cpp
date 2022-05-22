131ee3934865cd8a556b6cccd283a42c860c7b56
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-05-22 16:08:03 +0530

    Using Heapify
    https://www.codingninjas.com/codestudio/problems/build-min-heap_1171167?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar

---
+++ heaps/Build Min Heap.cpp
@@ -0,0 +1,26 @@
+void heapify(vector<int> &arr, int n, int i){
+    int smallest=i;
+	// 0-based indexing
+    int left=2*i+1;
+    int right=2*i +2;
+
+    if(left < n && arr[smallest] > arr[left]){
+        smallest=left;
+    }
+    if(right < n && arr[smallest] > arr[right]){
+        smallest=right;
+    }
+
+    if(smallest!=i){
+        swap(arr[smallest],arr[i]);
+        heapify(arr,n, smallest);  // recursive call
+    }
+}
+vector<int> buildMinHeap(vector<int> &arr)
+{
+	int n=arr.size();
+     for(int i=n/2-1;i>=0;i--){
+        heapify(arr,n,i);
+    }
+	return arr;
+}
