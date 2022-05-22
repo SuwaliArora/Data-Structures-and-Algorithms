9b17e91bab2e670fcc7e786dbd317fc5a144e7bb
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-05-22 15:47:17 +0530

    Implemented Heapify

--- heaps/insertionHeap.cpp
+++ heaps/insertionHeap.cpp
@@ -76,6 +76,25 @@ public:
         cout<<endl;   
     }
 };
+
+// implementation of heapify 
+void heapify(int arr[], int n, int i){
+    int largest=i;
+    int left=2*i;
+    int right=2*i +1;
+
+    if(left < n && arr[largest] < arr[left]){
+        largest=left;
+    }
+    if(right < n && arr[largest] < arr[right]){
+        largest=right;
+    }
+
+    if(largest!=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr,n, largest);  // recursive call
+    }
+}
  
 int main() {
     heap h;
@@ -87,5 +106,18 @@ int main() {
     h.print();
     h.deletefromHeap();
     h.print();
+    
+    int arr[6]={-1,54,53,55,52,50};
+    int n=5;
+    for(int i=n/2;i>0;i--){
+        heapify(arr,n,i);
+    }
+    cout<<"print the array "<<endl;
+    for (int i = 1; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
     return 0;
 }
