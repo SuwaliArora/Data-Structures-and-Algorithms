2698ce9433cf08b83e859bb1730033df251fad0c
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-05-24 15:12:03 +0530

    using priority queue
    https://leetcode.com/problems/kth-largest-element-in-an-array/

---
+++ heaps/Kth Largest Element in an Array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int i=0;i<k;i++){
+            pq.push(nums[i]);
+        }
+        
+        for(int i=k;i<n;i++){
+            if(nums[i]>pq.top()){
+                pq.pop();
+                pq.push(nums[i]);
+            }
+        }
+        return pq.top();
+    }
+};
