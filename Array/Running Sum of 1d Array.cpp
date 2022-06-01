d755776024c8b51ce86f042582917590d75a0477
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-06-01 21:30:12 +0530

    Simple Solution
    https://leetcode.com/problems/running-sum-of-1d-array/

---
+++ Array/Running Sum of 1d Array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            nums[i]=nums[i]+sum;
+            sum=nums[i];
+        }
+        return nums;
+    }
+};
+
+// T.C- O(n)
+// S.C- O(1)
