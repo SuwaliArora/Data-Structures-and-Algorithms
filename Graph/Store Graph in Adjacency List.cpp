95bcd0963ab41369c5e3396d955664e44d40b54c
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-06-23 23:00:17 +0530

    Weight of edges are given

--- Graph/Store Graph in Adjacency List.cpp
+++ Graph/Store Graph in Adjacency List.cpp
@@ -7,17 +7,17 @@ int main()
     int n,m;
     cin>>n>>m;
     
-    // declare adjacency list
-    vector<int> adj[n+1];
+    // declare adjacency list (weights of edges are also given)
+    vector<pair<int,int>> adj[n+1];
     
     // take edges as input
     for(int i=0;i<m;i++){
-        int u,v;
-        cin>>u>>v;
+        int u,v,wt;
+        cin>>u>>v>>wt;
         
         // mark 1 to denote edges between nodes
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].push_back({v,wt});
+        adj[v].push_back({u,wt});
     }
 
     return 0;
