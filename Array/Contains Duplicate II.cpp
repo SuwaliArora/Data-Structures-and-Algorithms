9f3afad689b20836db6ceb7f97330ddb238226e2
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-06-23 22:15:01 +0530

    Solution
    https://leetcode.com/problems/contains-duplicate-ii/

---
+++ Array/Contains Duplicate II.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<nums.size();i++){
+            if(mp.find(nums[i])!=mp.end()){
+                if(abs(mp[nums[i]]-i)<=k){
+                    return true;
+                }
+                // update map with new index for the number
+                 else{
+                      mp[nums[i]]=i;
+                 }
+            }
+            else{
+                mp[nums[i]]=i;
+            }
+        }
+        return false;
+    }
+};
