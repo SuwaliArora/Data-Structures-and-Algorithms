1547dcf27d17d7fc2c16dfdc73675cb0a1c95e34
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-06-25 11:41:53 +0530

    Creating and Printing Graph
    https://www.codingninjas.com/codestudio/problems/create-a-graph-and-print-it_1214551?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar

---
+++ Graph/Creating and Printing.cpp
@@ -0,0 +1,21 @@
+vector < vector < int >> printAdjacency(int n, int m, vector < vector < int >> & edges) {
+    // Write your code here.
+    // ans array will store adjacent nodes corresponding to i
+    vector<int> ans;
+    for(int i=0;i<m;i++){
+        int u=edges[i][0];
+        int v=edges[i][1];
+        
+        ans[u].push_back(v);
+        ans[v].push_back(u);
+    }
+    vector<vector<int>> adj(n);
+    for(int i=0;i<n;i++){
+        adj[i].push_back(i);
+        // entering neighbours
+        for(int j=0;j<ans[i].size();j++){
+            adj[i].push_back(ans[i][j]);
+        }
+    }
+    return adj;
+}
