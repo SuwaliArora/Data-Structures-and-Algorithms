55901a37c5b38f4d98ad964d39ff4ee98a1c67d0
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-07-10 22:29:13 +0530

    Solution
    https://leetcode.com/problems/counting-words-with-a-given-prefix/

---
+++ Strings/Counting Words With a Given Prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+       int count=0;
+       int n=pref.length();
+        for(auto it: words){
+            if(it.substr(0,n)==pref)
+                count++;
+        }
+        return count;
+    }
+};
