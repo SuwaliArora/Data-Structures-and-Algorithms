0b7c9e1b1daafbf14e268c635fa0ade5f2d2fd58
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-07-23 14:04:21 +0530

    Solution
    https://leetcode.com/problems/rotate-image/

---
+++ Matrix/Rotate Image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n=mat.size();
+        for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        reverse(mat[i].begin(),mat[i].end());
+    }    
+    }
+};
