3a4f713aa8506b5cbae7839c871caa1db0bdb0e3
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-09-03 18:02:36 +0530

    Solution
    https://leetcode.com/problems/guess-number-higher-or-lower/

---
+++ Array/Guess Number Higher or Lower.cpp
@@ -0,0 +1,24 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low=1,high=n,ans;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            
+            ans=guess(mid);
+            if(ans==0) return mid;
+            else if(ans==1) low=mid+1;
+            else if(ans==-1) high=mid-1;
+        }
+        return 0;
+    }
+};
