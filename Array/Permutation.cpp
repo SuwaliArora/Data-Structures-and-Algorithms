2d35958f73bac1366ad2267a55d152f6a9f0661e
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-09-05 17:32:43 +0530

    Using map, recursion
    https://leetcode.com/problems/permutations/

---
+++ Array/Permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<vector<int>>& ans,vector<int> ds,vector<int> freq){
+        if(ds.size()==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(!freq[i]) {
+                ds.push_back(nums[i]);
+                freq[i]=1;
+                solve(nums,ans,ds,freq);
+                freq[i]=0;
+                ds.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<int> freq(nums.size(),0);
+        solve(nums,ans,ds,freq);
+        return ans;
+    }
+};
