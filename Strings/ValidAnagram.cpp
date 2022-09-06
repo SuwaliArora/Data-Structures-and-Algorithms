3f9f96474f673fe9c2d0fe9e998253c0ca99dbd1
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-09-06 10:33:56 +0530

    using sorting
    https://leetcode.com/problems/valid-anagram/

--- Strings/ValidAnagram.cpp
+++ Strings/ValidAnagram.cpp
@@ -6,6 +6,23 @@ public:
         if(s.length()!=t.length()) return false;
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-        return (s==t);
+        return (s==t); // time complexity=O(n)
+    }
+};
+
+
+//method- 2
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) return false;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int n=s.length();
+        for(int i=0;i<n;i++){
+            if(s[i]!=t[i]) return false;
+        }
+        return true;
     }
 };
