d5c0de7e6037f798461862ea82046feda94cc832
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2022-11-09 19:17:08 +0530

    Solution
    https://leetcode.com/problems/online-stock-span/

---
+++ Stacks and Queue/Online Stock Span.cpp
@@ -0,0 +1,23 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>> st;
+    StockSpanner() {
+        
+    }
+    
+    int next(int price) {
+       int count=1;
+        while(st.size()>0 && price>=st.top().first){
+            count+=st.top().second;
+            st.pop();
+        }
+        st.push({price,count});
+        return st.top().second;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
